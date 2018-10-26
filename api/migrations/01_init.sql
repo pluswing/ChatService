1f8ff05103877d7a41dbfaeb351856f129bcfc10
Toshiyuki Saito <toshiyuki@pluswing.co.jp>
2018-10-26 23:05:46 +0900

    part 25

---
+++ api/migrations/01_init.sql
@@ -0,0 +1,62 @@
+CREATE TAVLE users (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    uid VARCHAR(50) NOT NULL
+    KEY (uid)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE user_messages (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    body TEXT NOT NULL,
+    created_at DATETIME NOT NULL,
+    operator_id BIGINT
+    KEY (user_id),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE operators (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    login_id VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    UNIQUE KEY (login_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE operator_messages (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    from_operator_id BIGINT NOT NULL,
+    to_operator_id BIGINT NOT NULL,
+    body TEXT NOT NULL,
+    created_at DATETIME NOT NULL,
+    KEY (from_operator_id),
+    KEY (to_operator_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE responsible (
+    user_id BIGINT NOT NULL,
+    operator_id BIGINT NOT NULL,
+    PRIMARY KEY (user_id, operator_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE user_messages ADD CONSTRAINT
+    user_messages_user_id FOREIGN KEY (user_id)
+    REFERENCES users(id) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+ALTER TABLE user_messages ADD CONSTRAINT
+    user_messages_operator_id FOREIGN KEY (operator_id)
+    REFERENCES operators(id) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+ALTER TABLE operator_messages ADD CONSTRAINT
+    operator_messages_from FOREIGN KEY (from_operator_id)
+    REFERENCES operators(id) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+ALTER TABLE operator_messages ADD CONSTRAINT
+    operator_messages_to FOREIGN KEY (to_operator_id)
+    REFERENCES operators(id) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+ALTER TABLE responsible ADD CONSTRAINT
+    responsible_user_id FOREIGN KEY (user_id)
+    REFERENCES users(id) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+ALTER TABLE responsible ADD CONSTRAINT
+    responsible_operator_id FOREIGN KEY (operator_id)
+    REFERENCES operators(id) ON DELETE RESTRICT ON UPDATE RESTRICT;
