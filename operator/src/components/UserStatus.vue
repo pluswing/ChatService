0cc7d88314ed8fe46026d68b95abb87ab0fc4160
Toshiyuki Saito <toshiyuki.saito.16@gmail.com>
2019-05-23 10:34:41 +0900

    part88

--- operator/src/components/UserStatus.vue
+++ operator/src/components/UserStatus.vue
@@ -1,27 +1,26 @@
 <template>
-  <v-card
-    class="mx-auto"
-    color="#26c6da"
-    :style="{'background-color': gbColor }"
-    dark
-    max-width="400"
-  >
-    <v-card-title>
-      <span class="title font-weight-light">{{ user.uid }}</span>
-    </v-card-title>
+  <v-badge v-model="showBadge" color="red" right overlap>
+    <template v-slot:badge>
+      <span>{{ user.arrival }}</span>
+    </template>
+    <v-card class="mx-auto" color="#26c6da" dark max-width="400">
+      <v-card-title>
+        <span class="title font-weight-light">{{ user.uid }}</span>
+      </v-card-title>
 
-    <v-card-text class="headline font-weight-bold">
-      <router-link :to="{ name: 'chat', params: { uid: user.uid }}">{{ user.message.message }}</router-link>
-    </v-card-text>
+      <v-card-text class="headline font-weight-bold">
+        <router-link :to="{ name: 'chat', params: { uid: user.uid }}">{{ user.message.message }}</router-link>
+      </v-card-text>
 
-    <v-card-actions>
-      <v-list-tile class="grow">
-        <v-layout align-center justify-end>
-          <span class="subheading">{{ user.message.date() }}</span>
-        </v-layout>
-      </v-list-tile>
-    </v-card-actions>
-  </v-card>
+      <v-card-actions>
+        <v-list-tile class="grow">
+          <v-layout align-center justify-end>
+            <span class="subheading">{{ user.message.date() }}</span>
+          </v-layout>
+        </v-list-tile>
+      </v-card-actions>
+    </v-card>
+  </v-badge>
 </template>
 
 <script lang="ts">
@@ -34,8 +33,8 @@ import { User } from '@/models/User';
 export default class UserStatus extends Vue {
   @Prop() private user!: User;
 
-  get gbColor(): string {
-    return this.user.arrival == 0 ? '#26c6da' : 'red';
+  get showBadge(): boolean {
+    return this.user.arrival > 0;
   }
 }
 </script>
