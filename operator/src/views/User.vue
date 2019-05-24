8900b870059e8a8f0e413f87ba29d13112d7a511
Toshiyuki Saito <toshiyuki.saito.16@gmail.com>
2019-05-24 09:54:54 +0900

    part89

--- operator/src/views/User.vue
+++ operator/src/views/User.vue
@@ -1,9 +1,34 @@
 <template>
   <v-app>
     <v-content>
-      <v-layout wrap>
-        <v-flex xs3 style="margin:10px;" v-for="user in users" :key="user.id">
-          <UserStatus :user="user"/>
+      <v-layout>
+        <v-flex xs9>
+          <v-layout wrap>
+            <v-flex xs3 v-for="user in users" :key="user.id">
+              <UserStatus style="margin:10px;" :user="user"/>
+            </v-flex>
+          </v-layout>
+        </v-flex>
+        <v-flex xs3>
+          <v-list two-line>
+            <template v-for="(item, index) in items">
+              <v-list-tile :key="item.title" avatar ripple @click="toggle(index)">
+                <v-list-tile-content>
+                  <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                  <v-list-tile-sub-title class="text--primary">{{ item.headline }}</v-list-tile-sub-title>
+                  <v-list-tile-sub-title>{{ item.subtitle }}</v-list-tile-sub-title>
+                </v-list-tile-content>
+
+                <v-list-tile-action>
+                  <v-list-tile-action-text>{{ item.action }}</v-list-tile-action-text>
+                  <v-icon v-if="selected.indexOf(index) < 0" color="grey lighten-1">star_border</v-icon>
+
+                  <v-icon v-else color="yellow darken-2">star</v-icon>
+                </v-list-tile-action>
+              </v-list-tile>
+              <v-divider v-if="index + 1 < items.length" :key="index"></v-divider>
+            </template>
+          </v-list>
         </v-flex>
       </v-layout>
     </v-content>
@@ -29,6 +54,40 @@ import UserApi from '../repositories/UserApi';
 export default class Home extends Vue {
   public users: User[] = [];
   @State('operator') public operator!: OperatorState;
+
+  public selected = [2];
+  public items = [
+    {
+      action: '15 min',
+      headline: 'Brunch this weekend?',
+      title: 'Ali Connors',
+      subtitle: 'I\'ll be in your neighborhood doing errands this weekend. Do you want to hang out?'
+    },
+    {
+      action: '2 hr',
+      headline: 'Summer BBQ',
+      title: 'me, Scrott, Jennifer',
+      subtitle: 'Wish I could come, but I\'m out of town this weekend.'
+    },
+    {
+      action: '6 hr',
+      headline: 'Oui oui',
+      title: 'Sandra Adams',
+      subtitle: 'Do you have Paris recommendations? Have you ever been?'
+    },
+    {
+      action: '12 hr',
+      headline: 'Birthday gift',
+      title: 'Trevor Hansen',
+      subtitle: 'Have any ideas about what we should get Heidi for her birthday?'
+    },
+    {
+      action: '18hr',
+      headline: 'Recipe to try',
+      title: 'Britta Holt',
+      subtitle: 'We should eat this: Grate, Squash, Corn, and tomatillo Tacos.'
+    }
+  ];
   private getusers = new GetUsers(new UserApi());
   private connection = new WebSocket('ws://localhost:3010/v1/chat/ws/');
 
